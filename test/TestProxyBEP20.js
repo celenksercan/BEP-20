6bb54674a09ee1d17d2db724675642edd610c60a
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-04 23:56:01 +0300

    Update TestProxyBEP20.js

--- test/TestProxyBEP20.js
+++ test/TestProxyBEP20.js
@@ -8,3 +8,8 @@ const ApproveAndCallFallBackTest = artifacts.require("ApproveAndCallFallBackTest
 
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+
+const fs = require('fs');
+
+let bep20TokenAddress;
