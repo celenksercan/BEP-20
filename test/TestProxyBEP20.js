6e3c572f003a212ec22113e54537d7c9aac3f25c
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-04 23:55:44 +0300

    Update TestProxyBEP20.js

--- test/TestProxyBEP20.js
+++ test/TestProxyBEP20.js
@@ -1,1 +1,7 @@
 const truffleAssert = require('truffle-assertions');
+
+const BEP20TokenImplementation = artifacts.require("BEP20TokenImplementation");
+// const BEP20UpgradeableProxy = artifacts.require("BEP20UpgradeableProxy");
+const BEP20TokenFactory = artifacts.require("BEP20TokenFactory");
+const BEP20TokenImplementationV1 = artifacts.require("BEP20TokenImplementationV1");
+const ApproveAndCallFallBackTest = artifacts.require("ApproveAndCallFallBackTest");
