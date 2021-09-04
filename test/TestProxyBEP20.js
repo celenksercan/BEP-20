9199cdda7abd5bf782f4374893b3a14e2a863839
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-04 23:55:54 +0300

    Update TestProxyBEP20.js

--- test/TestProxyBEP20.js
+++ test/TestProxyBEP20.js
@@ -5,3 +5,6 @@ const BEP20TokenImplementation = artifacts.require("BEP20TokenImplementation");
 const BEP20TokenFactory = artifacts.require("BEP20TokenFactory");
 const BEP20TokenImplementationV1 = artifacts.require("BEP20TokenImplementationV1");
 const ApproveAndCallFallBackTest = artifacts.require("ApproveAndCallFallBackTest");
+
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
